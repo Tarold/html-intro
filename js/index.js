733d1ec885a76e54d46b4c5d24719adbde7dfab7
‘Tarold’ <ltdecorer@gmail.com>
2022-10-30 23:09:52 +0200

    Add conditions an example

--- js/index.js
+++ js/index.js
@@ -1,202 +1,238 @@
-// JS - мова з динамічною типізацією, високорівнева, інтерпретована, мультипарадигмова, скриптова
+// if - розгалуження
 
-// js виконується браузером (движком)
+// if (умова) {
+//   гілка true;
+// } else {
+//   гілка false;
+// }
 
-// Стандарт ECMASctipt
+// Булевські змінні імунуємо з is...
 
-// Виведення в консоль
-console.log("Hello, world!");
+const isLogged = false;
 
-// Вивести в модальне вікно
-alert("Hello, world!");
+if (isLogged) {
+  console.log("You are logged in))))");
+} else {
+  console.log("Try again(((");
+}
 
-// Однорядковий коментар
-/*
-  Багаторядковий коментар
-*/
+// Task: Задана змінна
+// const isEnoughMoney = true;
+// Якщо грошей достатньо, вивести “Операцію схвалено)”
+// Якщо недостатньо, то вивести “Операцію відхилено(”
 
-// Змінні
+const isEnoughMoney = true;
 
-let a = 1;
-console.log("a =", a);
-a = a + 1;
-console.log("a =", a);
+if (isEnoughMoney) {
+  console.log("Операцію схвалено)))");
+} else {
+  console.log("Операцію не схвалено(((");
+}
 
-// для уникнення випадкового переприсвоювання важливої інф-ї
-// рекомендується МАКСИМАЛЬНО вживати const
-const pi = 3.14; // не можна переприсвоювати значення!
-console.log(pi);
+// Перевірити, чи скінченне число ввів користувач, і видати відповідне повідомлення.
 
-var b = 10; // старий стиль
-console.log("b =", b);
+const inputValue = Number(prompt("Input number")); // => 55, Infinity, NaN
 
-// Стиль іменування: camelCase
-// lowerCamelCase - типовий для простих змінних
-// UpperCamelCase
+if (Number.isFinite(inputValue)) {
+  console.log("number");
+} else {
+  console.log("not a finite number");
+}
 
-// PI, MATH_PI - для констант
+// Оператори порівняння
 
-// 1    a-z, A-Z, _, $
-// 2... a-z, A-Z, _, $, 0-9
+const age = 18;
 
-// Типи даних - примітивні (7)
-//            - object
+console.log("age > 18 :>> ", age > 18);
+console.log("age < 18 :>> ", age < 18);
+console.log("age >= 18 :>> ", age >= 18);
+console.log("age <= 18 :>> ", age <= 18);
 
-// string
-// 'string', "string", `string`
-const stringVariable1 = "string1";
-const stringVariable2 = "string2"; // подвійні лапки
-const stringVariable3 = `string3`;
-// "string2" - літерально задане значення
+// Нестрога рівність / нерівність - ПОГАНО!
+console.log("age == 18 :>> ", age == 18);
+console.log('age == "18" :>> ', age == "18");
+console.log("age != 18 :>> ", age != 18);
+console.log('age != "18" :>> ', age != "18");
 
-console.log('He says :"Hello"');
+// Строга / нерівність рівність
+console.log("age === 18 :>> ", age === 18);
+console.log('age === "18" :>> ', age === "18");
+console.log("age !== 18 :>> ", age !== 18);
+console.log('age !== "18" :>> ', age !== "18"); // 18!=='18'
 
-// number
-// 1, 22, -50, 10.5, 10e5, Infinity, -Infinity, 0 (+0, -0), NaN
+// Task: Якщо користовач повнолітній, то привітати, якщо ні - то доступ заборонений
 
-const numberVariable1 = Infinity;
-console.log("numberVariable1 :>> ", numberVariable1);
+const age = 20;
 
-const numberVariable2 = NaN;
-console.log("numberVariable2 :>> ", numberVariable2);
+if (age >= 18) {
+  console.log("Wellcome!");
+} else {
+  console.log("Access is denied");
+}
 
-const numberVariable3 = 10.5;
-console.log("numberVariable3 :>> ", numberVariable3);
+// Скорочена форма if
 
-console.log("10 / 0 :>> ", 10 / 0); // Infinity
-console.log("10 / -0 :>> ", 10 / -0); // -Infinity
-console.log('"abc" / 5 :>> ', "abc" / 5); // NaN - Not a Number
-// Number.MAX_VALUE
-// Number.MAX_SAFE_INTEGER
+// if (умова) {
+//   гілка true;
+// }
 
-// bigint
+// При вході перевірити, чи користувач повнолітній
+// Якщо ні - то видати попередження про обмежений доступ
+// Вивести привітання
 
-const bigintValue = 9999999999999999n;
-console.log("bigintValue :>> ", bigintValue);
+const age = 18;
 
-// boolean - value: true, false
+if (age < 18) {
+  console.log("Деякі функції будуть обмежені");
+}
 
-const boolValue = false;
-console.log("boolValue :>> ", boolValue);
+console.log("Вітаємо на сайті!)");
 
-// symbol
-const symbolValue = Symbol();
-console.log("symbolValue :>> ", symbolValue);
+// Task: если пользователь несовершеннолетний, то "Wellcome"
+// иначе если сертификат есть, то "Wellcome"
+//       иначе вакцинироваться
 
-// undefined - value: undefined
+const age = 18;
+const isVactinated = true;
 
-// const undValue = undefined
-let undValue;
-console.log("undValue :>> ", undValue);
+if (age < 18) {
+  console.log("Wellcome)");
+} else {
+  if (isVactinated) {
+    console.log("Wellcome)");
+  } else {
+    console.log("Please go to vactination");
+  }
+}
+// або
 
-// null - value: null
+// Розширена форма if
 
-const nullValue = null;
-console.log("nullValue :>> ", nullValue);
+// if (умова) {
+//   гілка true;
+// } else if (умова) {
+//   гілка ;
+// } else if (умова) {
+//   гілка ;
+// } else {
+//   гілка false;
+// }
 
-// не примітив / object
-const obj = {};
+if (age < 18) {
+  console.log("Wellcome");
+} else if (isVactinated) {
+  console.log("Wellcome");
+} else {
+  console.log("Please go to vactination");
+}
 
-// typeof значення
-console.log("typeof 5 :>> ", typeof 5); // 'number'
-console.log('typeof "5" :>> ', typeof "5"); // 'string'
-console.log("typeof NaN :>> ", typeof NaN); // 'number'
-console.log("typeof true :>> ", typeof true); // 'boolean'
-console.log("typeof null :>> ", typeof null); // 'object'
+// Task: Ввести 2 числа. Вивести з них більше
+// Ввести 2 числа. Вивести що вони рівні, а якщо ні, то більше з них
 
-// Операції
+const userInput1 = Number(prompt("Введіть перше число"));
+const userInput2 = Number(prompt("Введіть друге число"));
 
-const a1 = 1; // ініціалізація - присвоювання при оголошенні
-const b1 = 2;
+if (userInput1 > userInput2) {
+  console.log("більше число = ", userInput1);
+} else if (userInput1 < userInput2) {
+  console.log("більше число = ", userInput2);
+} else {
+  console.log("Вони еквівалентні", userInput1 + " = " + userInput2);
+}
 
-let c1 = null; // ініціалізація
-c1 = a1 + b1; // присвоювання
+// Умовні (булевські) операції
 
-console.log("c :>> ", c);
+// умова І умова   - умова && умова - age >= 18 && isVactinated
+// умова АБО умова - умова || умова - age < 18  || isVactinated
+// НЕ умова        - !умова         - !isVactinated
 
-console.log("1 + 2 =", 1 + 2);
-console.log("1 - 2 =", 1 - 2);
-console.log("1 * 2 =", 1 * 2);
-console.log("1 / 2 =", 1 / 2);
-console.log("1 % 2 =", 1 % 2); // остача від ділення: 7/2 = 3 (1)/2
-console.log("2 ** 2 =", 2 ** 2);
-console.log("Math.sqrt(9) :>> ", Math.sqrt(9));
-console.log("Math.abs(-10) :>> ", Math.abs(-10));
+const age = 18;
+const isVactinated = true;
 
-// Task: a = 3, b = 4; c - ?
-const a2 = 3;
-const b2 = 4;
-const c2 = Math.sqrt(a2 ** 2 + b2 ** 2);
-console.log("c :>> ", c2);
+if (age < 18 || isVactinated) {
+  console.log("Wellcome");
+} else {
+  console.log("Go to vactination");
+}
 
-// Task: squareSide = 5; S-?, P-?
-const squareSide = 5;
-const S = Math.pow(squareSide, 2); // squareSide ** 2;
-const P = squareSide * 4;
-console.log("P =", P);
-console.log("S =", S);
+console.group("Таблиця істинності &&:");
 
-// Короткі записи операцій
-let value = 10;
-value += 20; // value = value + 20;
-console.log("value :>> ", value);
+console.log("true && true :>> ", true && true); // => true, якщо всі true
+console.log("false && false :>> ", false && false);
+console.log("true && false :>> ", true && false);
+console.log("false && true :>> ", false && true);
 
-let value2 = 10;
-value2 -= 20; // value2 = value2 - 20;
-console.log("value2 :>> ", value2);
-// є аналогічні *=, /=
+console.groupEnd();
 
-// Інкремент і декремент
-// i = i + 1
-// i += 1
-// i++, ++i
+console.group("Таблиця істинності ||:");
 
-let i = 1;
-// постфіксний інкремент i++
-console.log("i++ :>> ", i++);
-console.log("i :>> ", i);
+console.log("true || true :>> ", true || true);
+console.log("false || false :>> ", false || false); // => false, якщо всі false
+console.log("true || false :>> ", true || false);
+console.log("false || true :>> ", false || true);
 
-let j = 1;
-// інфіксний інкремент ++j
-console.log("++j :>> ", ++j);
-console.log("j :>> ", j);
+console.groupEnd();
 
-let k = 1;
-// постфіксний декремент k--
-console.log(k--);
-console.log("k = ", k);
+console.group("Таблиця істинності !:");
 
-let l = 1;
-// інфіксний декремент --l
-console.log(--l);
-console.log("l = ", l);
+console.log("!true :>> ", !true);
+console.log("!false  :>> ", !false);
 
-// Пріоритетність операцій
-const result1 = 2 ** (3 ** 2); // 512
+console.groupEnd();
 
-// Приведення типу (явне)
+// Task: Вивести нагадування про знижку, якщо парне число випало на п'ятницю
+const day = "Tuesday";
+const date = 15;
 
-console.log("Number(5) :>> ", Number(5));
-console.log('Number("5") :>> ', Number("5"));
-console.log('Number("abc") :>> ', Number("abc"));
-// Task: привести true, false, null, undefined, 55n, Symbol() до Number
-console.log("true =", Number(true));
-console.log("false =", Number(false));
-console.log("null =", Number(null));
-console.log("undefined =", Number(undefined));
-console.log("55n =", Number(55n));
-// console.log('Symbol() =', Number(Symbol())); // помилка!!
+if (day === "Friday" && date % 2 === 0) {
+  console.log("EVEN FRIDAY Sale!");
+}
 
-// Неявне приведення типів
-console.log("true + 10 :>> ", true + 10);
+// Task: Вивести інфо про знижку, якщо людина пенсійгого віку або має неповнолітню дитину
+const userAge = 80;
+const userChildAge = 25;
 
-// Введення даних
-const inputValue = Number(prompt("Введіть число:")); // 50
-const result2 = inputValue + inputValue; // Без явного приведення типів: '50' + '50' = '5050'
-console.log("result :>> ", result2);
+if (userAge >= 60 || userChildAge < 5) {
+  console.log("Znigki)");
+}
 
-// Task: ввести 2 числа і вивести їх суму
-const a3 = +prompt("input value");
-const b3 = Number(prompt("input value"));
-const result3 = a3 + b3;
-console.log("You summ is ", result3);
+// Task: якщо неповнолітній, то обмежити доступ
+const isAdult = true;
+
+// isAdult !== true, isAdult === false
+if (!isAdult) {
+  console.log("Доступ обмежено");
+}
+
+// Task: перевітири, чи число є 0 чи ні
+
+const a = -5;
+if (a) {
+  console.log("no 0");
+} else {
+  console.log(0);
+}
+
+// falsy values: 0, null, undefined, '', false, NaN
+
+const value = "";
+if (value !== undefined || value !== null || value !== "") {
+  console.log("ok");
+}
+// або
+// Use Case: чи прийшло значення
+if (value) {
+  console.log("ok");
+}
+
+// Task: Перевірити, що прийшло число і воно не NaN
+
+const value = "dgdgdfg";
+
+if (typeof value === "number" && !Number.isNaN(value)) {
+  console.log("number not NaN");
+}
+// або
+if (!isNaN(value)) {
+  console.log("number not NaN");
+}
