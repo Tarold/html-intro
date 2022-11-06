08ff61f7c7427318e2830aa125f43271699d82b7
‘Tarold’ <ltdecorer@gmail.com>
2022-11-06 21:19:36 +0200

    Added functions JS Exercises

--- js/index.js
+++ js/index.js
@@ -1,135 +1,89 @@
-// ФУНКЦІЇ
-
-// Повторне використання коду
-
-// Оголошення функції (function declaration)
-// function functionName(){
-//   function body
-// }
-
-// functionName() - виклик функції
-
-// Оголошення функції
-function sayHello() {
-  console.log('Hello');
+//0
+function isWorkingAgePerson(age) {
+  return age >= 16 && age <= 65 ? true : false;
 }
+console.group(0);
+console.log('isWorkingAgePerson(20)', isWorkingAgePerson(20));
+console.log('isWorkingAgePerson(4)', isWorkingAgePerson(4));
+console.log('isWorkingAgePerson(88)', isWorkingAgePerson(88));
+console.groupEnd();
 
-sayHello(); // - виклик функції
-sayHello(); // - виклик функції
-sayHello(); // - виклик функції
-
-// Передача значень до функції
-
-const userName = 'Ivo';
-
-function sayHelloToUser(name) {
-  // name - параметр, вважається локальною змінною функції
-  // локальні змінні не видимі ззовні функції
-  console.log(`Hello, ${name}`);
-  // return undefined; - якщо функція не містить return, то вона повертає undefined
-}
-// значення userName копіюється в параметр name функції
-sayHelloToUser(userName);
-
-// Повернення значень із функції
-
-function sumAB(a, b) {
-  return a + b;
-}
-
-const sum = sumAB(4, 5);
-console.log('sum :>> ', sum);
-console.log('sumAB(15,30) :>> ', sumAB(15, 30));
-
-// Task: Написати ф-ю для розрахунку числа в степені 4: 5=>625; 2=>16
-
-function pow4(num) {
-  return num ** 4;
-}
-console.log(pow4(5));
-
-// або
-
-function fourthPower(digit) {
-  let result = Math.pow(digit, 4);
-  return result; // одна відповідальність!!!
-}
-
-if (Number.isFinite(userDigit)) {
-  alert(`${userDigit} ^ 4 = ${fourthPower(userDigit)}`);
-} else {
-  alert('You did not enter a number! Error!');
-}
-
-// Піднесення числа до довільного степеня
-function pow(a, b) {
-  return a ** b;
-}
-const numbers = pow(2, 3);
-console.log(numbers);
-
-const userDigit = Number(prompt('Enter number'));
-
-// Task: Написати функцію для розрахунку суми чисел від 1 (*m) до n
-// 5   => 1+2+3+4+5 = 15
-
-function sumToMaxValue(limit) {
-  let sum = 0;
-  for (let i = 1; i <= limit; i++) {
-    sum = sum + i;
+//1
+function isPrimeNumber(number) {
+  if (number === 1) {
+    return false;
+  } else if (number > 1) {
+    for (let i = 2; i < number; i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
   }
-  return sum;
-}
 
-console.log(sumToMaxValue(5));
-
-// *5,7 => 5 + 6 + 7 = 18
-
-function sumFromMinToMaxValue(lowLimit, upperLimit) {
-  let sum = 0;
-  for (let i = lowLimit; i <= upperLimit; i++) {
-    sum = sum + i;
+  // check if number is less than 1
+  else {
+    return false;
   }
-  return sum;
 }
+console.group(1);
+console.log('isPrimeNumber(7)', isPrimeNumber(7));
+console.log('isPrimeNumber(8)', isPrimeNumber(8));
+console.groupEnd();
 
-console.log(sumFromMinToMaxValue(5, 10));
+//2
+const checkMultiplicity = function (dividend, divisor) {
+  return dividend % divisor === 0 ? true : false;
+};
+console.group(2);
+console.log('checkMultiplicity(25, 5)', checkMultiplicity(25, 5));
+console.log('checkMultiplicity(15,3)', checkMultiplicity(15, 3));
+console.log('checkMultiplicity(15, 5)', checkMultiplicity(15, 5));
+console.log('checkMultiplicity(15, 4)', checkMultiplicity(15, 4));
+console.groupEnd();
 
-// Task: Написати функцію, яка повертає true для повнолітнього користувача,
-// false - для неповнолітнього
+//3
+/**
+ * Перевірка можливості існування трикутника
+ * @param {number} a Сторона повина бути number більше 0
+ * @param {number} b Сторона повина бути number більше 0
+ * @param {number} c Сторона повина бути number більше 0
+ * @returns {boolean} можливості існування трикутника в boolean
+ */
+const checkTriangleExists = function (a, b, c) {
+  return a + b > c && a + c > b && b + c > a && a > 0 && b > 0 && c > 0
+    ? true
+    : false;
+};
+console.group(3);
+console.log('checkTriangleExists(2, 3, 4)', checkTriangleExists(2, 3, 4));
+console.log('checkTriangleExists(2, -3, 4)', checkTriangleExists(2, -3, 4));
+console.log('checkTriangleExists(2, 3, 40)', checkTriangleExists(2, 3, 40));
+console.groupEnd();
 
-function isAdult(age) {
-  return age >= 18;
+//4
+/**
+ * Функція знаходить повну площу паралелепіпеда
+ * @param {number} a сторона паралелепіпеда
+ * @param {number} b сторона паралелепіпеда
+ * @param {number} c сторона паралелепіпеда
+ *
+ * @return {number} Повна площадь поверхні паралелепіпеда
+ *
+ * Функція знаходить повну площу конуса
+ * @param {number} R Радіус конуса
+ * @param {number} l Висота конуса
+ *
+ * @return {number} Повна площадь поверхні конуса
+ */
+
+function areaCalculation(a, b, c) {
+  if (c === undefined) {
+    return Math.round(Math.PI * a * (a + b), -2);
+  }
+  return Math.round(2 * (a * b + b * c + a * c), -2);
 }
-console.log(isAdult(17));
-
-// або
-
-const userAge = Number(prompt('Enter Your age'));
-
-function isAdult(age) {
-  return age >= 18;
-}
-
-if (isAdult(userAge)) {
-  alert('You are adult');
-} else {
-  alert('You are not adult');
-}
-
-// Task: Написати функцію, яка за рядком "Ivo Smith" сформує рядок з ініціалами ("IS")
-
-function getInitials(userFullName) {
-  // знайти індекс пробіла
-  const spaceIndex = userFullName.indexOf(' ');
-  // сконкатенувати перший символ і перший символ після пробілу
-
-  const initials =
-    userFullName.substring(0, 1) +
-    userFullName.substring(spaceIndex + 1, spaceIndex + 2);
-
-  return initials;
-}
-
-console.log('getInitials("") :>> ', getInitials('Ivo Smith'));
-console.log('getInitials("") :>> ', getInitials('Wally Brown'));
+console.group(4);
+console.log('areaCalculation cone', areaCalculation(10, 50));
+console.log('areaCalculation parallelepiped', areaCalculation(10, 10, 10));
+console.groupEnd();
