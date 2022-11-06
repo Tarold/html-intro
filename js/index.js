3b28c9b601b50479af26b8852b645b7156de9cde
‘Tarold’ <ltdecorer@gmail.com>
2022-11-06 16:32:13 +0200

    Added functions

--- js/index.js
+++ js/index.js
@@ -1,50 +1,135 @@
-// const greeting = 'Hello';
-// const userName = prompt('Input name');
-// console.log(greeting + ', ' + userName);
-// console.log(`${greeting}, ${userName}`); // шаблонний літерал
+// ФУНКЦІЇ
 
-// 1*1=1
-// 1*2=2
-// 1*3=3
-// ....
-// 2*1=2
-// 2*2=4
-// 2*3=6
+// Повторне використання коду
 
-// for (let i = 1; i <= 10; i++) {
-//   for (let j = 1; j <= 10; j++) {
-//     // console.log(i + ' * ' + j + ' = ' + i * j); // переписати на шаблонний літерал
-//     console.log(`${i} * ${j} = ${i * j}`);
-//   }
+// Оголошення функції (function declaration)
+// function functionName(){
+//   function body
 // }
 
-// Пользователь имеет 3 попытки ввести пароль
-// Если пароль верен, то вывести "Пароль верен"
-// Иначе "Пароль неверен"
+// functionName() - виклик функції
 
-// let userPassword = '';
-// const PASSWORD = 'qwerty';
-// const TRY_LIMIT = 3;
-// let correntTryNumber = 1;
+// Оголошення функції
+function sayHello() {
+  console.log('Hello');
+}
 
-// do {
-//   userPassword = prompt('Input password').trim();
-// } while (userPassword !== PASSWORD && correntTryNumber++ < TRY_LIMIT);
+sayHello(); // - виклик функції
+sayHello(); // - виклик функції
+sayHello(); // - виклик функції
 
-// if (userPassword === PASSWORD) {
-//   console.log('Password correct');
-// } else {
-//   console.log('Password incorrect');
-// }
+// Передача значень до функції
 
-const PASSWORD = 'qwerty';
-const TRY_LIMIT = 3;
-let userPassword = '';
-let correntTryNumber = 1;
-let isPasswordCorrect = false;
+const userName = 'Ivo';
 
-do {
-  isPasswordCorrect = prompt('Input password').trim() === PASSWORD;
-} while (!isPasswordCorrect && correntTryNumber++ < TRY_LIMIT);
+function sayHelloToUser(name) {
+  // name - параметр, вважається локальною змінною функції
+  // локальні змінні не видимі ззовні функції
+  console.log(`Hello, ${name}`);
+  // return undefined; - якщо функція не містить return, то вона повертає undefined
+}
+// значення userName копіюється в параметр name функції
+sayHelloToUser(userName);
 
-console.log(`Password ${isPasswordCorrect ? 'correct' : 'incorrect'}`);
+// Повернення значень із функції
+
+function sumAB(a, b) {
+  return a + b;
+}
+
+const sum = sumAB(4, 5);
+console.log('sum :>> ', sum);
+console.log('sumAB(15,30) :>> ', sumAB(15, 30));
+
+// Task: Написати ф-ю для розрахунку числа в степені 4: 5=>625; 2=>16
+
+function pow4(num) {
+  return num ** 4;
+}
+console.log(pow4(5));
+
+// або
+
+function fourthPower(digit) {
+  let result = Math.pow(digit, 4);
+  return result; // одна відповідальність!!!
+}
+
+if (Number.isFinite(userDigit)) {
+  alert(`${userDigit} ^ 4 = ${fourthPower(userDigit)}`);
+} else {
+  alert('You did not enter a number! Error!');
+}
+
+// Піднесення числа до довільного степеня
+function pow(a, b) {
+  return a ** b;
+}
+const numbers = pow(2, 3);
+console.log(numbers);
+
+const userDigit = Number(prompt('Enter number'));
+
+// Task: Написати функцію для розрахунку суми чисел від 1 (*m) до n
+// 5   => 1+2+3+4+5 = 15
+
+function sumToMaxValue(limit) {
+  let sum = 0;
+  for (let i = 1; i <= limit; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(sumToMaxValue(5));
+
+// *5,7 => 5 + 6 + 7 = 18
+
+function sumFromMinToMaxValue(lowLimit, upperLimit) {
+  let sum = 0;
+  for (let i = lowLimit; i <= upperLimit; i++) {
+    sum = sum + i;
+  }
+  return sum;
+}
+
+console.log(sumFromMinToMaxValue(5, 10));
+
+// Task: Написати функцію, яка повертає true для повнолітнього користувача,
+// false - для неповнолітнього
+
+function isAdult(age) {
+  return age >= 18;
+}
+console.log(isAdult(17));
+
+// або
+
+const userAge = Number(prompt('Enter Your age'));
+
+function isAdult(age) {
+  return age >= 18;
+}
+
+if (isAdult(userAge)) {
+  alert('You are adult');
+} else {
+  alert('You are not adult');
+}
+
+// Task: Написати функцію, яка за рядком "Ivo Smith" сформує рядок з ініціалами ("IS")
+
+function getInitials(userFullName) {
+  // знайти індекс пробіла
+  const spaceIndex = userFullName.indexOf(' ');
+  // сконкатенувати перший символ і перший символ після пробілу
+
+  const initials =
+    userFullName.substring(0, 1) +
+    userFullName.substring(spaceIndex + 1, spaceIndex + 2);
+
+  return initials;
+}
+
+console.log('getInitials("") :>> ', getInitials('Ivo Smith'));
+console.log('getInitials("") :>> ', getInitials('Wally Brown'));
