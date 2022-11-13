5e60f193dd3b8292eb0537a2436acc5ee82b0dc5
‘Tarold’ <ltdecorer@gmail.com>
2022-11-13 06:57:23 +0200

    Added Array exercices
    forEach, filter, map, findIndex, some, every

--- js/index.js
+++ js/index.js
@@ -1,195 +1,159 @@
-// const usersList = {
-//   1: { id: 1, name: 'Test' },
-//   2: { id: 2, name: 'Ivo' },
-// };
+// 0 Створити числовий масив та проініціалізувати його (*випадковими числами).
 
-// Array - впорядкована колекція
-
-// const arr1 = []; // Синтаксичний цукор
-// const arr2 = new Array();
-
-// const arr3 = [1, 2, 3, 4];
-
-// // arr[i] - доступ до елемента під номером i
-// // arr.length - довжина масива
-
-// // const users = [
-// //   { id: 1, name: 'Test' },
-// //   { id: 2, name: 'Ivo' },
-// // ];
-
-// // написати функцію для виводу елементів масиву
-// // function printArray(arr) {
-// //   for (let i = 0; i < arr.length; i++) {
-// //     console.log('arr[i] :>> ', arr[i]);
-// //   }
-// // }
-
-// // printArray(arr3);
-
-// // написати функцію для обчислення суми елементів масиву
-// // функція приймає масив і повертає його суму
-
-// function countSum(arr) {
-//   let sum = 0;
-//   for (let i = 0; i < arr.length; i++) {
-//     console.log(`arr[${i}] :>> `, arr[i]);
-//     sum += arr[i];
-//   }
-//   return sum;
-// }
-
-// console.log('countSum(arr3) :>> ', countSum(arr3));
-
-// const arr4 = arr3; // погано, бо копіюється посилання
-
-// // Копіювання масивів
-// const arr5 = Array.from(arr3);
-
-// const arr6 = [...arr3];
-
-// // Методи масивів
-// arr3[arr3.length] = 5;
-// console.log('arr3 :>> ', arr3);
-
-// arr3.push(6);
-// console.log('arr3 :>> ', arr3);
-
-// // ввести з консолі n елемнтів масиву
-
-// function inputArray(n) {
-//   const arr = [];
-//   for (let i = 0; i < n; i++) {
-//     arr.push(Number(prompt('input item')));
-//   }
-//   return arr;
-// }
-
-// // console.log('inputArray(5); :>> ', inputArray(5));
-
-// console.log('arr3[arr3.length-1] :>> ', arr3[arr3.length - 1]);
-
-// console.log('arr3.pop() :>> ', arr3.pop()); // повертає і видаляє останній елемент
-
-// console.log('arr3.shift(10) :>> ', arr3.shift()); // повертає і видаляє перший елемент
-
-// arr3.unshift(10); // додає на початок масива елемент
-
-// //  shift i unshift використовувати в крайніх випадках!
-
-// // slice/splice
-// const singleNumbers = arr3.slice(3);
-// console.log('singleNumbers :>> ', singleNumbers);
-
-// //скопіювати однозначні числа
-// const arr10 = [10, 15, 2, 3, 5, 122, 155];
-// const singleNumbers2 = arr10.slice(2, -2);
-// console.log('singleNumbers2 :>> ', singleNumbers2);
-
-// // видалити з arr10 5-ку
-// arr10.splice(4, 1);
-// console.log('splice', arr10);
-
-// const arr11 = [1, 2, 3];
-// const arr12 = [10, 11, 12];
-// const arr13 = arr11.concat(arr12);
-// console.log('arr11.concat(arr12) :>> ', arr13);
-
-// перебір масиву
-// for..in - проходе по індексах (властивостях)
-
-// for (key in arr13) {
-//   console.log(`arr[${key}]`, arr13[key]);
-// }
-
-// // for..of - проходе по значеннях
-// for (item of arr13) {
-//   console.log('item :>> ', item);
-// }
-
-// Task: перебрати for..of масив
-// const users = [
-//   { id: 1, name: 'Test' },
-//   { id: 2, name: 'Ivo' },
-// ];
-
-// for (const item of users) {
-//   console.log('user:', item);
-// }
-
-// Методи перебору масивів
-// const myCb = function (x) {
-//   console.log('x :>> ', x);
-// };
-
-// function f(cb, a) {
-//   cb(a);
-// }
-
-// f(myCb, 5);
-
-// forEach виконує коллбек послідовно для всіх елементів масиву
-
-const arr20 = [1, 3, 5, 7];
-
-function printArray(currentValue, index, array) {
-  console.log(
-    `currentValue: ${currentValue}; 
-     index: ${index}; 
-     array: ${array}.`
-  );
+function random10() {
+  return Math.round(Math.random() * 10);
 }
 
-arr20.forEach(printArray);
+const arrFirst = [];
 
-const users = [
-  { id: 1, name: 'Test' },
-  { id: 2, name: 'Ivo' },
-];
+for (let index = 0; index < 10; index++) {
+  arrFirst.push(random10());
+}
+console.log('arr', arrFirst);
 
-users.forEach(function (user) {
-  user.isLiked = false;
+let arrSecond = [...arrFirst];
+// 1 Видалити останній і початковий елемент з масиву, додати елемент до початку і кінця.
+// Firts realization
+
+arrFirst.shift();
+arrFirst.unshift(random10());
+arrFirst.pop();
+arrFirst.push(random10());
+
+// second realization
+
+arrSecond = [random10(), ...arrSecond.slice(1, -1), random10()];
+console.log('arr first realization', arrFirst);
+console.log('arr second realization', arrSecond);
+
+// 2 Вивести розмір масиву.
+console.log('arrFirst.length', arrFirst.length);
+// 3 Зробити копію масиву.
+const arrFirstCopy = Array.from(arrSecond);
+
+// // Не використовувати методи перебору масивів (forEach, filter, map, findIndex), а використати цикли
+
+// 4 Вивести елементи з парними індексами.
+let evenIndex = 0;
+console.group('only even index');
+do {
+  console.log(`arrFirst[${evenIndex}] `, arrFirst[evenIndex]);
+  evenIndex += 2;
+} while (evenIndex < arrFirst.length);
+console.groupEnd();
+
+// 5 Вивести лише парні елементи (парні числа діляться на 2 без залишку).
+console.group('only couples');
+
+for (const element in arrFirst) {
+  if (element % 2 === 0) {
+    console.log(element);
+  }
+}
+console.groupEnd();
+
+// 6 Вивести індекси нульових елементів (елемент дорівнює нулю).
+
+const awesomeArray = [];
+for (let index = 0; index < 10; index++) {
+  if (Math.round(Math.random()) > 0) {
+    awesomeArray.push(0);
+  } else {
+    awesomeArray.push(random10());
+  }
+}
+
+console.log('awesomeArray', awesomeArray);
+
+console.group('indices of zero elements');
+for (let index = 0; index < awesomeArray.length; index++) {
+  if (awesomeArray[index] == 0) {
+    console.log('index: ', index);
+  }
+}
+console.groupEnd();
+
+// 7 Підрахувати кількість нульових елементів.
+
+let zeroCount = 0;
+
+for (let index = 0; index < awesomeArray.length; index++) {
+  if (awesomeArray[index] == 0) {
+    zeroCount++;
+  }
+}
+console.log('zeroCount', zeroCount);
+
+// // Методи перебору масивів (forEach, filter, map, findIndex, *some, *every).
+// 8 Отримати новий масив із заданого, який міститиме лише ненульові числа (-1, 5, 0, 9, -10 => -1, 5, 9, -10).
+const excellentArray = [-1, 5, 0, 9, -10];
+console.log(
+  'excellentArray',
+  excellentArray.filter((element) => {
+    return element != 0;
+  })
+);
+
+// 9 Отримати новий масив їх заданого, який міститиме всі елементи вихідного, поділені на 100 (99, 5, 0, 9, 30 => 0.99, 0.05, 0, 0.09, 0.3).
+
+const prettyArray = [99, 5, 0, 9, 30];
+console.log(
+  'prettyArray',
+  prettyArray.map((element) => element / 100)
+);
+
+// 10 Вивести елементи масиву, зведені у куб.
+
+const magicalArray = [];
+for (let index = 0; index < 10; index++) {
+  magicalArray.push(random10());
+}
+console.log('magicalArray', magicalArray);
+
+console.group('magicalArray**2');
+magicalArray.forEach((element, index) => {
+  console.log(`element[${index}]`, element ** 2);
 });
+console.groupEnd();
 
-// вивести users за допомогою forEach
+// 11 Визначити індекс елемента, квадрат якого дорівнює 100, і видалити його, або видати діагностичне повідомлення, якщо такого елементу не існує.
+let treasureArray = [2, 5, 10, 5, 8];
+const indexReturn = treasureArray.findIndex((element) => {
+  return element ** 2 === 100;
+});
+switch (indexReturn) {
+  case -1:
+    alert('no such element exists');
+    break;
 
-function printUsers(currentValue) {
-  console.log(currentValue);
+  default:
+    treasureArray = treasureArray.filter(
+      (item, index) => index !== indexReturn
+    );
+    break;
 }
-users.forEach(printUsers);
+console.log('treasureArray', treasureArray);
 
-// map => новий масив з елементами, які повертаються з колбеку для кожного елемента вихідного масиву
-
-const arr21 = [10, 20, 30];
-
-function mapArray(currentValue) {
-  return currentValue ** 2;
+// 12 *Перевірити, чи всі елементи масиву є парними числами (* або простими числами).
+const primeArray = [2, 3, 5, 7, 11];
+function isPrime(number) {
+  if (number > 1) {
+    for (let i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
 }
+console.log('every prime arrFirst', arrFirst.every(isPrime));
+console.log('every prime clearArray', primeArray.every(isPrime));
 
-const arr22 = arr21.map(mapArray);
-
-// filter -
-
-const arr23 = [1, 2, 3, 4];
-
-function isOdd(item) {
-  return item % 2 === 1;
+// 13 *Перевірити, чи є у масиві бодай один від'ємний елемент.
+const someNegArray = [5, 2, 3, -1, 5];
+function isNegative(number) {
+  return number < 0;
 }
-
-const arr24 = arr23.filter(isOdd);
-
-const arr25 = [-5, -5, 3, -11, -55]; // відфільтрувати додатні, тобто [1,3]
-
-function isPositive(item) {
-  return item > 0;
-}
-const arr26 = arr25.filter(isPositive);
-console.log('arr26', arr26);
-
-// findIndex
-
-function find3(item) {
-  return item === -5;
-}
-
-const foundedIndex = arr25.findIndex(find3); // поверне індекс першого знайденого, або -1 якщо не знайшов
+console.log('some negative arrFirst', arrFirst.some(isNegative));
+console.log('some negative someNegArray', someNegArray.some(isNegative));
