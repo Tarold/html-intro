5a89acfc0356728c9d32670b49e4430cf6a982b4
‘Tarold’ <ltdecorer@gmail.com>
2022-11-20 00:44:36 +0200

    Added error handling examples

--- js/index.js
+++ js/index.js
@@ -1,31 +1,92 @@
-function counter() {
-  function getNum(comment) {
-    let num = Number(prompt(`Enter ${comment} number`));
-    while (isNaN(num)) {
-      num = Number(
-        prompt(`Sent message is not number!\nEnter ${comment} number`)
-      );
-    }
-    return num;
-  }
-
-  let i = getNum('start');
-  const increase = getNum('increase');
-
-  return function () {
-    debugger;
-    return (i += increase);
-  };
+try {
+  // код, який потенційно може викликати помилки
+} catch (err) {
+  // блок, куди буде передане куревання, якщо виникла вбудована помилка
+  // або згенерувалася вручну в throw
 }
 
-const counter1 = counter();
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
-console.log('counter1() :>> ', counter1());
+// throw генерує помилку (виняткову ситуацію/исключительную ситуацию/exception)
+// вона передається в catch:
+// throw new Error
+//          ||
+//          \/
+// catch  (error)
 
-const counter2 = counter();
-console.log('counter2() :>> ', counter2());
-console.log('counter2() :>> ', counter2());
+function fact(n) {
+  if (typeof n !== 'number') {
+    throw new TypeError('n must be number value'); // на catch
+  }
+  if (n < 0 || !Number.isSafeInteger(n)) {
+    throw new RangeError('n must be positive integer value'); // на catch
+  }
+  if (n === 0) return 1;
+  return n * fact(n - 1);
+}
 
-console.log('counter1() :>> ', counter1());
+try {
+  fact(-5);
+} catch (err) {
+  console.dir(err);
+  if (err instanceof TypeError) {
+    console.log('Input incorrect type');
+  } else if (err instanceof RangeError) {
+    console.log('Value must be pisitive integer');
+  } else {
+    console.log('Something went wrong');
+  }
+}
+
+// Task: написати функцію для валідації імені користувача (4-32, string)
+// якщо значення валідне, то повернути його
+// якщо ні, то згенерувати помилку
+
+function validateName(value) {
+  if (typeof value !== 'string') {
+    throw new TypeError('name must be string');
+  }
+  if (value.length < 4 || value.length > 32) {
+    throw new RangeError('name must be 4-32 characters');
+  }
+  return value;
+}
+
+try {
+  const validatedName = validateName('Test');
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.log('name must be');
+  } else if (error instanceof RangeError) {
+    console.log('error');
+  } else {
+    console.log('error');
+  }
+}
+
+// Task: Реалізувати функцію, яка приймає вік,
+// повертає вік, якщо параметр відповідає вимогам,
+// інакше генерує помилку
+
+function validateAge(value) {
+  if (typeof value !== 'number') {
+    throw new TypeError('age must be a number');
+  }
+  if (value < 0 || value > 150 || !Number.isSafeInteger(value)) {
+    throw new RangeError(
+      'Age must be bigger than 0 but smaller than 150 and it must be integer value.'
+    );
+  }
+  return value;
+}
+
+try {
+  const validatedAge = validateAge(0.5);
+  console.log('validatedAge :>> ', validatedAge);
+} catch (error) {
+  if (error instanceof TypeError) {
+    console.log('error : ', error);
+  } else if (error instanceof RangeError) {
+    console.log('error : ', error);
+  } else {
+    console.log('something went wrong.');
+  }
+}
