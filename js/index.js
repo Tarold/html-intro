ad5545fca5b9708c976761b408bb32f35c78e1ae
‘Tarold’ <ltdecorer@gmail.com>
2022-11-20 00:46:37 +0200

    Added classes

--- js/index.js
+++ js/index.js
@@ -1,66 +1,82 @@
-// Деструктуроване присвоєння
-
-// Об'єктів
-function f({
-  name: userName = 'Anonymous',
-  title = 'pan',
-  phone = '+380123456789',
-}) {
-  // const name = params.name
-  // const title = params.title;
-  // const phone = params.phone;
-  // або
-  // диструктуроване просвоєння
-  // const { name, title, phone } = params;
-  // або в списку параметрів
-
-  console.log('in f :>> ', userName);
-  console.log('in f :>> ', title);
-  console.log('in f :>> ', phone);
+function User1(name, surname, age, isMale, email, isSubscribed) {
+  this.firstName = name;
+  this.lastName = surname;
+  this.age = age;
+  this.isMale = isMale;
+  this.email = email;
+  this.isSubscribed = isSubscribed;
 }
 
-f({ title: 'pan' });
-
-// імена змінних мають відповідати іменам властивостей об'єкта або вказані після :
-const book = {
-  author: {
-    firstName: 'Test',
-    lastName: 'Testovich',
-  },
-  price: 12,
+User1.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
 };
 
-const {
-  author: { firstName, lastName },
-  price,
-} = book;
-
-console.log('price :>> ', price);
-console.log('firstName :>> ', firstName);
-console.log('lastName :>> ', lastName);
-
-// Масивів
-// Оголошені змінні співставляються за порядком елементів
-const [firstItem, , thirdItem] = [1, 2, 3];
-console.log('firstItem :>> ', firstItem);
-console.log('thirdItem :>> ', thirdItem);
-
-const user1 = {
-  name: 'Test',
-  age: 23,
-  emails: ['test1@test.com', 'test2@test.com'],
-  isMale: true,
+User1.isUser1 = function (obj) {
+  return obj instanceof User1;
 };
 
-// Task: name, age, email (email1, email2)
-// перейменувати name -> userName, age -> userAge
+// Класи - синтаксичний цукор над вбудованими в жс функціями-конструкторами і прототипами
+class User {
+  constructor(name, surname, age, isMale, email, isSubscribed) {
+    this.firstName = name;
+    this.lastName = surname;
+    this.age = age; // спрацьовує сеттер
+    this.isMale = isMale;
+    this.email = email;
+    this.isSubscribed = isSubscribed;
+  }
+  // сеттер - спеціального вигляду метод
+  set age(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('age must be number');
+    }
+    if (value < 0 || value > 150 || !Number.isInteger(value)) {
+      throw new RangeError('age must be 0-130 integer');
+    }
+    this._age = value;
+  }
+  get age() {
+    return this._age;
+  }
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+  // статичний метод - працює для класа, а не для його екземпляра
+  static isUser(obj) {
+    return obj instanceof User;
+  }
+}
 
-const {
-  name: userName,
-  age: userAge,
-  emails: [firstMail, secondMail],
-  isMale,
-} = user1;
-console.log('userName :>> ', userName);
-console.log('userAge :>> ', userAge);
-console.log('emails :>> ', secondMail);
+// constructor - аналог функції-конструктора
+const user11 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
+
+try {
+  const user1 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
+  console.log('user1.getFullName() :>> ', user1.getFullName());
+  console.log('User.isUser({}) :>> ', User.isUser({}));
+  console.log('User.isUser(user1) :>> ', User.isUser(user1));
+  user1.age = 26; // спрацьовує сеттер (дозволяє звернутися до метода і встановити значення за синтаксисом властивості)
+  console.log('user.age :>> ', user1.age); // спрацьовує геттер (дозволяє звернутися до метода і отримати значення за синтаксисом властивості)
+} catch (err) {
+  console.log('err :>> ', err);
+}
+
+// Task: реалізувати клас для телефонів (марка, модель, колір, ціна, рік випуску)
+// реалізувати метод для розрахунку віку телефону
+// створити екземпляр класу, викликати для нього метод
+
+class Phone {
+  constructor(mark, model, color, price, year) {
+    this.mark = mark;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+    this.year = year;
+  }
+  getAge() {
+    return new Date().getFullYear() - this.year;
+  }
+}
+
+const telephone1 = new Phone('Nokia', '3310', 'black', 2000, 2010);
+console.log(telephone1.getAge());
