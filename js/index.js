4b667e10b29fba4ff37adcb59553c75caa5cda7b
‘Tarold’ <ltdecorer@gmail.com>
2022-11-20 03:22:25 +0200

    Added JS. Error handling exercise

--- js/index.js
+++ js/index.js
@@ -1,82 +1,27 @@
-function User1(name, surname, age, isMale, email, isSubscribed) {
-  this.firstName = name;
-  this.lastName = surname;
-  this.age = age;
-  this.isMale = isMale;
-  this.email = email;
-  this.isSubscribed = isSubscribed;
+function pow(base, exponent) {
+  if (typeof base !== 'number' || typeof exponent !== 'number') {
+    throw new TypeError('argument must be number value'); // на catch
+  }
+  if (
+    base < 0 ||
+    !Number.isSafeInteger(base) ||
+    exponent < 0 ||
+    !Number.isSafeInteger(exponent)
+  ) {
+    throw new RangeError('argument must be positive integer value'); // на catch
+  }
+  if (exponent === 1) return base;
+  return base * pow(base, exponent - 1);
 }
-
-User1.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-User1.isUser1 = function (obj) {
-  return obj instanceof User1;
-};
-
-// Класи - синтаксичний цукор над вбудованими в жс функціями-конструкторами і прототипами
-class User {
-  constructor(name, surname, age, isMale, email, isSubscribed) {
-    this.firstName = name;
-    this.lastName = surname;
-    this.age = age; // спрацьовує сеттер
-    this.isMale = isMale;
-    this.email = email;
-    this.isSubscribed = isSubscribed;
-  }
-  // сеттер - спеціального вигляду метод
-  set age(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('age must be number');
-    }
-    if (value < 0 || value > 150 || !Number.isInteger(value)) {
-      throw new RangeError('age must be 0-130 integer');
-    }
-    this._age = value;
-  }
-  get age() {
-    return this._age;
-  }
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  // статичний метод - працює для класа, а не для його екземпляра
-  static isUser(obj) {
-    return obj instanceof User;
-  }
-}
-
-// constructor - аналог функції-конструктора
-const user11 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
 
 try {
-  const user1 = new User('Test', 'Testovich', 25, true, 'test@gmail.com', true);
-  console.log('user1.getFullName() :>> ', user1.getFullName());
-  console.log('User.isUser({}) :>> ', User.isUser({}));
-  console.log('User.isUser(user1) :>> ', User.isUser(user1));
-  user1.age = 26; // спрацьовує сеттер (дозволяє звернутися до метода і встановити значення за синтаксисом властивості)
-  console.log('user.age :>> ', user1.age); // спрацьовує геттер (дозволяє звернутися до метода і отримати значення за синтаксисом властивості)
+  console.log(pow(-5, 3));
 } catch (err) {
-  console.log('err :>> ', err);
-}
-
-// Task: реалізувати клас для телефонів (марка, модель, колір, ціна, рік випуску)
-// реалізувати метод для розрахунку віку телефону
-// створити екземпляр класу, викликати для нього метод
-
-class Phone {
-  constructor(mark, model, color, price, year) {
-    this.mark = mark;
-    this.model = model;
-    this.color = color;
-    this.price = price;
-    this.year = year;
-  }
-  getAge() {
-    return new Date().getFullYear() - this.year;
+  if (err instanceof TypeError) {
+    console.log('Input incorrect type');
+  } else if (err instanceof RangeError) {
+    console.log('Value must be pisitive integer');
+  } else {
+    console.log('Something went wrong');
   }
 }
-
-const telephone1 = new Phone('Nokia', '3310', 'black', 2000, 2010);
-console.log(telephone1.getAge());
