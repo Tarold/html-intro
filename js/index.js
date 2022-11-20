b91bf35cb4d8e819526172a384f73acca5d7c0e4
‘Tarold’ <ltdecorer@gmail.com>
2022-11-21 00:54:58 +0200

    Added iterable

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+// symbol
+
 const symbolVar = Symbol();
 
 console.log('symbolVar :>> ', symbolVar);
 
+// symbol як ключ в об'єкті
 const privatKey = Symbol();
 
 const user = {
@@ -11,6 +14,7 @@ const user = {
 
 console.log('user[Symbol()] :>> ', user[privatKey]);
 
+// symbol для генерації унікальних значень
 const THEMES = {
   LIGHT: Symbol('light'),
   DARK: Symbol('dark'),
@@ -28,3 +32,64 @@ switch (theme) {
   default:
     console.log(' unexpected value ');
 }
+
+// Symbol.iterator
+
+// 1 for..of
+
+// 2 деструктуризація колекцій
+const [first, second] = [1, 2];
+
+// 3 spread operator
+// створення копії колекції
+// конкатенація колекцій
+
+// для масивів:
+const arr1 = [1, 2, 3];
+
+// копіювання масивів
+const arr2 = [...arr1];
+
+function f() {
+  const args = [...arguments];
+}
+f(1, 2, 3);
+
+// конкатенація масивів
+const arr3 = [9, 8, 7];
+const arr13 = [...arr1, ...arr3];
+
+// розширення масиву в аргументи функції
+const arr10 = [1, 2, 7];
+
+// f1(arr10[0], arr10[1], arr10[2]);
+// або
+f1(...arr10);
+
+function f1(a, b, c) {
+  console.log('a,b,c :>> ', a, b, c);
+}
+
+// для об'єктів (не через Symbol.iterator)
+const user1 = { name: 'Test', age: 30 };
+
+// копіювання
+const user2 = { ...user1 };
+
+// злиття об'єктів в один
+const user3 = { isMale: true, isSubscribed: false };
+const user13 = { ...user1, ...user3 };
+
+// Ex. отримання набору опцій з користувацьких та дефолтних
+sendRequest({ url: 'http://google.com' });
+
+function sendRequest(options) {
+  const defaultOptions = {
+    url: 'http://localhost',
+    port: 3000,
+  };
+  const resultOptions = { ...defaultOptions, ...options };
+  // ...
+}
+
+// 4 Array.from() - створює масив з псевдомасива або ітерованої колекції
