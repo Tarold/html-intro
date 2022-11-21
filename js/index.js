3625465e6d84b30f74e778776bf77518988af38c
‘Tarold’ <ltdecorer@gmail.com>
2022-11-21 04:01:18 +0200

    Added JS. Map exercises

--- js/index.js
+++ js/index.js
@@ -1,147 +1,35 @@
-// Map
-
-// Колекція по ключу, яка може як ключ містити будь-який тип
-
-// Властивості/методи:
-// size
-// set(key, value) => map
-// get(key) => value
-// has(key) => boolean
-// delete(key) => boolean
-// clear()
-
-// keys() => MapKeyIterator
-// values() => MapValueIterator
-// enrties() => MapEntryIterator
-// forEach()
-
-// Створення
-const m1 = new Map();
-const m2 = new Map([
-  ['name', 'tel1'],
-  ['name2', 'tel2'],
-]);
-
-// m1.Test = '+380123456789'
-m1.set('Test', '+380123456789')
-  .set('Test2', '+380123456788')
-  .set('Test3', '+380123456787');
-
-// m1.Test
-console.log('m1.get("Test") :>> ', m1.get('Test'));
-
-// Test in m1
-console.log('m1.has("Test") :>> ', m1.has('Test'));
-console.log('m1.has("Test1") :>> ', m1.has('Test1'));
-
-// delete m1.Test3
-m1.delete('Test3');
-console.log('m1 :>> ', m1);
-
-// m1.clear();
-// console.log('m1 :>> ', m1);
-
-// Перебір мапи
-for (const item of m1) {
-  console.log('item :>> ', item);
-}
-
-const myArrKeys = [...m1.keys()];
-
-// Task: Отримати масив значень з мапи (values()) - ['+380123456789', '+380123456789']
-const myArrValues = [...m1.values()];
-
-const myArrEntries = [...m1.entries()];
-
-// Task: Отримати мапу з об'єкта
-const o2 = {
-  ivo1: 'tel1',
-  ivo2: 'tel2',
-};
-const map = new Map(Object.entries(o2));
-
-// Example: перекладач. Словник міститься в мапі
-const vocabulary = new Map();
-vocabulary.set('dog', 'собака');
-vocabulary.set('cat', 'кіт');
-vocabulary.set('run', 'бігти');
-
-const engPhrase = 'dog run white cat';
-
-// function translate(engPhrase) {
-//   // розділити фразу на слова
-//   const engWords = engPhrase.split(' ');
-
-//   // якщо в словнику є слово, то перкласти його, якщо ні, то залишити як є
-//   function engToUkr(e) {
-//     if (vocabulary.has(e)) {
-//       return vocabulary.get(e);
-//     }
-//     return e;
-//   }
-//   const ukrWords = engWords.map(engToUkr);
-
-//   // з'єднати слова в один рядок
-//   const ukrPhrase = ukrWords.join(' ');
-//   return ukrPhrase;
-// }
-// або
-function translate(engPhrase) {
-  return engPhrase
-    .split(' ')
-    .map((e) => (vocabulary.has(e) ? vocabulary.get(e) : e))
-    .join(' ');
-}
-// або
-// const translate = engPhrase =>
-//   engPhrase
-//     .split(' ')
-//     .map(e => (vocabulary.has(e) ? vocabulary.get(e) : e))
-//     .join(' ');
-
-//---------------------------------------------------------------------
-// Об'єкти як ключі
-
-const user = { name: 'Test' };
-const mes = {
-  [user]: ['hello', ')))'],
-};
-
-const m10 = new Map();
-m10.set(user, ['hello', ')))']);
-
-console.log('m10.get(user) :>> ', m10.get(user));
-
-//-----------------------------------------------------------------
-const usersWithMessages2 = [
-  { id: 1, name: 'Test1', message: 'hello', date: new Date() },
-  { id: 1, name: 'Test1', message: 'how are you', date: new Date() },
-  { id: 2, name: 'Test2', message: 'hi)', date: new Date() },
-  { id: 2, name: 'Test2', message: 'fine)', date: new Date() },
+const arr = [
+  [1, 'first'],
+  [3, 'third'],
 ];
 
-const users2 = [];
+//Створіть колекцію Map з цього масиву.
+const m1 = new Map(arr);
+console.log('m3 :>> ', m1);
 
-usersWithMessages2.forEach((um) => {
-  if (!users2.find((u) => um.id === u.id)) {
-    users2.push({ id: um.id, name: um.name });
-  }
-});
+const m2 = new Map();
+for (let item of arr) {
+  m2.set(...item);
+}
+console.log('m2 :>> ', m2);
 
-const messages32 = usersWithMessages2.map((um) => ({
-  message: um.message,
-  date: um.date,
-}));
+//Отримайте список ключів та значень окремо.
+const arrKeys = [...m1.keys()];
+const arrValues = [...m1.values()];
 
-const uToM = new Map();
+console.log('arrKeys :>> ', arrKeys);
+console.log('arrValues :>> ', arrValues);
 
-users2.forEach((u) =>
-  uToM.set(
-    u,
-    usersWithMessages2
-      .filter((um) => um.id === u.id)
-      .map((um) => ({ message: um.message, date: um.date }))
-  )
-);
+//Отримайте кількість елементів.
+console.log('m1.size :>> ', m1.size);
 
-users2.forEach((u) => console.log('object32 :>> ', uToM.get(u)));
+//Додати та видалити елемент
+m1.set(2, 'second');
+console.log('m1.has(2) :>> ', m1.has(2));
+
+m1.delete(2);
+console.log('m1.has(2) :>> ', m1.has(2));
+
+//Здійсніть пошук за ключом
+console.log('m1.has(3) :>> ', m1.has(3));
+console.log('m1.get(3) :>> ', m1.get(3));
