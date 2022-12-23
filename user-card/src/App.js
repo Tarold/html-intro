2b4a807be534c32a34c4a7b4d6e5304868f1c7a3
tarold <ltdecorer@gmail.com>
2022-12-23 14:46:01 +0200

    Added card with props

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
 import UserCard from './components/UserCard';
 import './App.css';
 
 function App() {
+  const userData = {
+    name: 'Emma Watson',
+    tag: '@EmmaWatson',
+    gender: 'female',
+    tweets: 1337,
+    following: 561,
+    followers: 718,
+    photo:
+      'https://cdn.britannica.com/29/215029-050-84AA8F39/British-actress-Emma-Watson-2017.jpg',
+  };
+
   return (
     <div className="App">
-      <UserCard />
+      <UserCard data={userData} />
     </div>
   );
 }
--- user-card/src/components/UserCard/index.jsx
+++ user-card/src/components/UserCard/index.jsx
@@ -1,18 +1,51 @@
-import './card.css';
+import styles from './card.module.css';
 
-function UserCard() {
+function UserCard({ data }) {
+  const { name, tag, tweets, following, followers, gender, photo } = data;
+
   return (
-    <div className="userCard">
-      <div className="headCard">
-        <h2>Emma Watson</h2>
-        <a href="@EmmaWatson">@Emma Watson</a>
-      </div>
-      <div className="bottomCard">
-        <div className="infoSection">
-          <p>Views</p>
-          <p>1.1337</p>
-        </div>
-      </div>
+    <div className={styles.userCard}>
+      <UserHeadCard name={name} tag={tag} gender={gender} photo={photo} />
+      <UserBottomCard
+        tweets={tweets}
+        following={following}
+        followers={followers}
+      />
+    </div>
+  );
+}
+
+function UserHeadCard({ name, tag, gender, photo }) {
+  const genderColor = gender === 'male' ? { color: 'blue' } : { color: 'pink' };
+
+  return (
+    <div
+      className={styles.headCard}
+      style={{
+        backgroundImage: `url(${photo}`,
+      }}
+    >
+      <h2 style={genderColor}>{name}</h2>
+      <a href={tag}>{tag}</a>
+    </div>
+  );
+}
+
+function UserBottomCard({ tweets, following, followers }) {
+  return (
+    <div className={styles.bottomCard}>
+      <UserCardStats nameStat="Tweets" stat={tweets} />
+      <UserCardStats nameStat="following" stat={following} />
+      <UserCardStats nameStat="followers" stat={followers} />
+    </div>
+  );
+}
+
+function UserCardStats({ nameStat, stat }) {
+  return (
+    <div className={styles.infoSection}>
+      <p>{nameStat}</p>
+      <p>{stat}</p>
     </div>
   );
 }
