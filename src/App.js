9842a5b3cf56edf1fee73676cff068ed319c3460
tarold <ltdecorer@gmail.com>
2022-12-24 19:59:14 +0200

    Added ViewPortParams

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import StopWatch from './StopWatch';
+import { Component } from 'react';
+import ViewPortParams from './components/ViewPortParams';
 
-function App() {
-  return <StopWatch />;
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isVisible: false,
+    };
+  }
+
+  handleClick = () => {
+    const { isVisible } = this.state;
+    this.setState({ isVisible: !isVisible });
+  };
+
+  render() {
+    const { isVisible } = this.state;
+    return (
+      <>
+        <button onClick={this.handleClick}>
+          {isVisible ? 'Unmount' : 'Mount'}
+        </button>
+        {isVisible && <ViewPortParams />}
+      </>
+    );
+  }
 }
 
 export default App;
+
+// new Counter().render();
--- src/components/Counter/index.js
+++ src/components/Counter/index.js
@@ -4,20 +4,26 @@ class Counter extends Component {
   constructor(props) {
     super(props);
 
+    // початковий стан
     this.state = {
       count: 0,
     };
   }
 
   increment = () => {
+    // setState(зміни стану) => перерендер сторінки
     this.setState({ count: this.state.count + 1 });
   };
+
+  // додати кнопку - і прописати обробник decrement
   decrement = () => {
     this.setState({ count: this.state.count - 1 });
   };
+
   render() {
     return (
       <>
+        <Count count={this.state.count} />
         <div>{this.state.count}</div>
         <button onClick={this.decrement}>-</button>
         <button onClick={this.increment}>+</button>
@@ -27,3 +33,7 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+function Count({ count }) {
+  return <div>Props: {count}</div>;
+}
