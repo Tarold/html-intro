5d50cc24caa6807615add6d4e1b34aafdbc37362
tarold <ltdecorer@gmail.com>
2022-12-25 18:45:59 +0200

    Added userListItem in exept file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-import { Component } from 'react';
-import AnimalSlider from './components/AnimalSlider';
+import UsersList from './components/UserList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isVisible: false,
-    };
-  }
-
-  handleClick = () => {
-    const { isVisible } = this.state;
-    this.setState({ isVisible: !isVisible });
-  };
-
-  render() {
-    const { isVisible } = this.state;
-    return (
-      <>
-        <button onClick={this.handleClick}>
-          {isVisible ? 'Unmount' : 'Mount'}
-        </button>
-        {isVisible && <AnimalSlider />}
-      </>
-    );
-  }
+function App(props) {
+  return <UsersList />;
 }
 
 export default App;
