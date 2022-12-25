7da9599d0ac0484f9241a1f8f83ff547ddf863d0
tarold <ltdecorer@gmail.com>
2022-12-25 16:04:54 +0200

    Added stopwatch React exemple

--- src/components/StopWatch/index.jsx
+++ src/components/StopWatch/index.jsx
@@ -3,30 +3,52 @@ import { Component } from 'react';
 class StopWatch extends Component {
   constructor(props) {
     super(props);
-    console.log('constructor');
-    this.state = { count: 0 };
+
+    this.state = {
+      count: new Date(0, 0, 0, 0, 0, 0, 0),
+    };
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
+    this.start();
   }
-
-  componentDidUpdate() {
-    console.log('componentDidUpdate');
-  }
-
+  componentDidUpdate() {}
   componentWillUnmount() {
-    console.log('componentWillUnmount');
+    this.stop();
   }
+
+  tick = () => {
+    const newCount = new Date(this.state.count.valueOf());
+
+    newCount.setSeconds(newCount.getSeconds() + 1);
+    this.setState({ count: newCount });
+  };
+
+  start = () => {
+    if (!this.id) {
+      this.id = setInterval(this.tick, 1000);
+    }
+  };
+
+  stop = () => {
+    clearInterval(this.id);
+    this.id = null;
+  };
+
+  reset = () => {
+    this.setState({ count: new Date(0, 0, 0, 0, 0, 0, 0) });
+  };
 
   render() {
-    console.log('render');
     const { count } = this.state;
+
     return (
-      <>
-        <div>{count}</div>
-        <button onClick={() => this.setState({ count: count + 1 })}>+</button>
-      </>
+      <article>
+        <div>{count.toLocaleTimeString('en-GB')}</div>
+        <button onClick={this.start}>Start</button>
+        <button onClick={this.stop}>Stop</button>
+        <button onClick={this.reset}>Reset</button>
+      </article>
     );
   }
 }
