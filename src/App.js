097b7161c55d700ea1799014361cc1aa8f7f5efe
tarold <ltdecorer@gmail.com>
2023-01-15 18:09:07 +0200

    Added context ex

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import UsersLoader from './components/UsersLoader';
+import { createContext } from 'react';
 
-const App = (props) => {
+const DataContext = createContext();
+
+function App() {
+  const data = 'data007';
+
   return (
-    <>
-      <UsersLoader />
-    </>
+    <DataContext.Provider value={data}>
+      <Child />
+    </DataContext.Provider>
   );
-};
+}
 
 export default App;
+
+function Child() {
+  return (
+    <div>
+      <ChildChild />
+    </div>
+  );
+}
+
+function ChildChild() {
+  return (
+    <DataContext.Consumer>
+      {(data1) => <div>I am ChildChild: {data1}</div>}
+    </DataContext.Consumer>
+  );
+}
