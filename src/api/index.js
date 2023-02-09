4f9a72f33ad7ba5b3130a1adc68ff72635849d8c
tarold <ltdecorer@gmail.com>
2023-02-09 15:08:07 +0200

    Added List React Redux with Api

--- src/api/index.js
+++ src/api/index.js
@@ -4,19 +4,11 @@ const httpClient = axios.create({
   baseURL: 'https://ullhgy.sse.codesandbox.io/'
 })
 
-// httpClient
-//   .get('/contacts')
-//   .then(data => console.log('data :>> ', data.data))
-//   .catch(e => console.log('e :>> ', e))
+export const getPurchases = () => httpClient.get('/contacts')
 
 export const createNewPurchase = values => httpClient.post('/contacts', values)
 
-// httpClient
-//   .patch('/contacts/3599dfff-f8e1-4b88-a540-af5351ebecfe', { name: 'NewName' })
-//   .then(data => console.log('data :>> ', data.data))
-//   .catch(e => console.log('e :>> ', e))
+export const updatePurchase = (id, values) =>
+  httpClient.patch(`/contacts/${id}`, values)
 
-// httpClient
-//   .delete('/contacts/832c32d3-2dea-45b4-b162-c277b4d53b1b')
-//   .then(data => console.log('data :>> ', data.data))
-//   .catch(e => console.log('e :>> ', e))
+export const deletePurchase = id => httpClient.delete(`/contacts/${id}`)
--- src/store/slices/purchasesSlice.js
+++ src/store/slices/purchasesSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import { createNewPurchase } from '../../api'
+import * as API from '../../api'
 
 const PURCHASES_SLICE_NAME = 'purchases'
 
@@ -7,10 +7,46 @@ export const createPurchase = createAsyncThunk(
   `purchases/create`,
   async (values, thunkAPI) => {
     try {
-      const response = await createNewPurchase(values)
+      const response = await API.createNewPurchase(values)
       return response.data
     } catch (e) {
-      return thunkAPI.rejectWithValue(e)
+      return thunkAPI.rejectWithValue({ message: e.message })
+    }
+  }
+)
+
+export const getPurchases = createAsyncThunk(
+  `${PURCHASES_SLICE_NAME}/get`,
+  async (payload, thunkAPI) => {
+    try {
+      const response = await API.getPurchases()
+      return response.data
+    } catch (e) {
+      return thunkAPI.rejectWithValue({ message: e.message })
+    }
+  }
+)
+
+export const deletePurchase = createAsyncThunk(
+  `${PURCHASES_SLICE_NAME}/delete`,
+  async (payload, thunkAPI) => {
+    try {
+      await API.deletePurchase(payload)
+      return payload
+    } catch (e) {
+      return thunkAPI.rejectWithValue({ message: e.message })
+    }
+  }
+)
+
+export const updatePurchase = createAsyncThunk(
+  `${PURCHASES_SLICE_NAME}/update`,
+  async ({ id, values }, thunkAPI) => {
+    try {
+      const response = await API.updatePurchase(id, values)
+      return response.data
+    } catch (e) {
+      return thunkAPI.rejectWithValue({ message: e.message })
     }
   }
 )
@@ -24,6 +60,7 @@ const purchasesSlice = createSlice({
   },
   reducers: {},
   extraReducers: builder => {
+    // CREATE
     builder.addCase(createPurchase.pending, state => {
       state.isFetching = true
       state.error = null
@@ -33,6 +70,48 @@ const purchasesSlice = createSlice({
       state.isFetching = false
     }) // action.payload = response.data
     builder.addCase(createPurchase.rejected, (state, action) => {
+      state.error = action.payload
+      state.isFetching = false
+    })
+    // GET
+    builder.addCase(getPurchases.pending, (state, action) => {
+      state.isFetching = true
+      state.error = null
+    })
+    builder.addCase(getPurchases.fulfilled, (state, action) => {
+      state.purchases.push(...action.payload)
+      state.isFetching = false
+    })
+    builder.addCase(getPurchases.rejected, (state, action) => {
+      state.error = action.payload
+      state.isFetching = false
+    })
+    // DELETE
+    builder.addCase(deletePurchase.pending, state => {
+      state.isFetching = true
+      state.error = null
+    })
+    builder.addCase(deletePurchase.fulfilled, (state, action) => {
+      state.purchases = state.purchases.filter(p => p.id !== action.payload)
+      state.isFetching = false
+    })
+    builder.addCase(deletePurchase.rejected, (state, action) => {
+      state.error = action.payload
+      state.isFetching = false
+    })
+    // UPDATE
+    builder.addCase(updatePurchase.pending, state => {
+      state.isFetching = true
+      state.error = null
+    })
+    builder.addCase(updatePurchase.fulfilled, (state, action) => {
+      const foundIndex = state.purchases.findIndex(
+        p => p.id === action.payload.id
+      )
+      state.purchases[foundIndex] = action.payload
+      state.isFetching = false
+    })
+    builder.addCase(updatePurchase.rejected, (state, action) => {
       state.error = action.payload
       state.isFetching = false
     })
