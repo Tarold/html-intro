ebb28b3b1da24ac73801c1ff302947ec8ccdb92a
tarold <ltdecorer@gmail.com>
2023-02-25 19:46:42 +0200

    Added Node methods exports

---
+++ index.js
@@ -0,0 +1,4 @@
+const { sum, mult } = require('./math');
+
+console.log('sum :>> ', sum(3, 4));
+console.log('mult :>> ', mult(3, 4));
---
+++ math.js
@@ -0,0 +1,13 @@
+// 1
+// const sum = (a, b) => a + b;
+// const mult = (a, b) => a * b;
+
+// module.exports = { sum, mult };
+
+//2
+// export const sum = (a, b) => a + b;
+// export const mult = (a, b) => a * b;
+
+//3
+module.exports.sum = (a, b) => a + b;
+module.exports.mult = (a, b) => a * b;
