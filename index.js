67a0fd51192ed228472a9696e79145dcd6f5339f
tarold <ltdecorer@gmail.com>
2023-02-26 17:33:43 +0200

    Added simple server

--- index.js
+++ index.js
@@ -1,64 +1,23 @@
-const os = require('os');
-const fs = require('fs');
+const { readFile } = require('fs/promises');
+const http = require('http');
 
-// console.log('os.platform() :>> ', os.platform());
-// вивести інфо про ЦП
-// console.log('os.cpus() :>> ', os.cpus());
+const httpServer = http.createServer((request, response) => {
+  console.log('request :>> ', request.url);
+  console.log('response :>> ', response.method);
 
-const fileText = fs.readFileSync('./math.js', { encoding: 'utf-8' });
-// console.log('fileText :>> ', fileText);
-
-// error-first contract in node.js native modules (Callback API)
-fs.readFile('./math.js', { encoding: 'utf-8' }, (err, data) => {
-  if (err) {
-    // console.log('err :>> ', err);
-  } else {
-    // console.log('data :>> ', data);
-  }
-});
-
-// "прочитати" директорію і вивести в консоль результат
-fs.readdir('.', (err, data) => {
-  if (err) {
-    console.log('err', err);
-  } else {
-    data
-      // відфільтровуємо тільки js-файли
-      .filter(f => /^.*\.js$/.test(f))
-      // кожен файл читаємо і виводимо в консоль
-      .forEach(f =>
-        fs.readFile(f, { encoding: 'utf-8' }, (err, data) => {
-          if (err) {
-            console.log('err :>> ', err);
-          } else {
-            // console.log('data :>> ', data);
-          }
-        })
-      );
-  }
-});
-
-// Переписати в синхронному вигляді
-
-try {
-  const files = fs.readdirSync('.');
-  // console.log('Files in directory:');
-  files
-    .filter(f => /^.*\.js$/.test(f))
-    .forEach(f => {
-      try {
-        const data = fs.readFileSync(f, { encoding: 'utf-8' });
-        // console.log('Data from file:', f);
-        // console.log(data);
-      } catch (err) {
-        // console.log('Error reading file:', f, err);
-      }
+  readFile('./index.html', { encoding: 'utf-8' })
+    .then(data => {
+      response.statusCode = 200;
+      response.setHeader('Context-Type', 'plain/text');
+      response.end(data);
+    })
+    .catch(err => {
+      response.statusCode = 500;
+      response.setHeader('Context-Type', 'plain/text');
+      response.end('File was not read');
     });
-} catch (err) {
-  console.log('Error reading directory:', err);
-}
-//
-console.log('__filename :>> ', __filename);
-console.log('__dirname :>> ', __dirname);
+});
 
-console.log('process :>> ', process.env);
+httpServer.listen(5000, 'localhost', () => {
+  console.log('Server is listening port 5000, host "localhost"');
+});
