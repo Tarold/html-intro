f47e22ede52dfd5816aaa6cf8dc0e0f7923a193f
tarold <ltdecorer@gmail.com>
2023-03-07 07:48:34 +0200

    Refactor model

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const express = require('express');
+const { ContactDB } = require('./models');
+
 const app = express();
 
-const { TasksDB, defaultValues } = require('./db');
-const DB = new TasksDB(defaultValues);
+app.use(express.json());
 
-app
-  .use(express.json())
-  .get('/tasks', (req, res) => {
-    res.send(DB.getTasks());
-  })
-  .post('/createTask', (req, res) => {
-    res.send(DB.createTask(req.body));
-  });
+app.get('/', (req, res) => {
+  res.send('app )))');
+});
+
+app.get('/contacts/', (req, res) => {
+  const contacts = ContactDB.getContacts();
+  res.status(200).send(contacts);
+});
+
+app.post('/contacts', (req, res) => {
+  const createdContact = ContactDB.createContact(req.body);
+  res.status(201).send(createdContact);
+});
+
+// params, req
+// GET localhost:5000/contacts/10?results=10&page=5
+// :id - параметр маршрута
+// ?results=10&page=5 - параметри рядка запиту
+app.get('/contacts/:id', (req, res) => {
+  const {
+    params: { id },
+    query: { results, page },
+  } = req;
+
+  console.log('req.params :>> ', req.params);
+  console.log('req.query :>> ', req.query);
+
+  res.status(200).send('OK');
+});
 
 module.exports = app;
+
+// звернутися до всіх замовлень певного користувача, зокрема до виконаних
+// (від загального до конретного)
+// users/5/orders?isDone=true
+// прописати обробник, в якому витягти id користувача і дані для фільтру замовлень
+app.get('/users/:id/orders', (req, res) => {
+  const {
+    params: { id },
+    query: { isDone },
+  } = req;
+
+  console.log('id, isDone :>> ', id, isDone);
+  res.status(200).send('OK');
+});
