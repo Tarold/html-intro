33ca746fbdb1f957bf363173d63c0d4de7b53edc
tarold <ltdecorer@gmail.com>
2023-04-03 18:28:09 +0300

    Add phone model, migration

---
+++ migrations/20230403141640-create-phone.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('phones', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      model: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+      brand: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+      year: {
+        type: Sequelize.SMALLINT,
+        allowNull: false,
+      },
+      ram: {
+        type: Sequelize.STRING(32),
+        allowNull: false,
+      },
+      processor: {
+        type: Sequelize.STRING(64),
+        allowNull: false,
+      },
+      screenSize: {
+        type: Sequelize.STRING(32),
+        allowNull: false,
+      },
+      nfc: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('phones');
+  },
+};
---
+++ models/phone.js
@@ -0,0 +1,42 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Phone extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Phone.init(
+    {
+      model: { type: DataTypes.STRING(64), allowNull: false },
+      brand: { type: DataTypes.STRING(64), allowNull: false },
+      year: {
+        type: DataTypes.SMALLINT,
+        allowNull: false,
+        validate: { is: /^\d{4}$/, max: Number(new Date.getFullYear()) },
+      },
+      ram: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        validate: { is: /^\d+ \w+$/ },
+      },
+      processor: { type: DataTypes.STRING(64), allowNull: false },
+      screenSize: {
+        type: DataTypes.STRING(32),
+        allowNull: false,
+        validate: { is: /^\d+x\d+$/ },
+      },
+      nfc: { type: DataTypes.BOOLEAN, defaultValue: false },
+    },
+    {
+      sequelize,
+      modelName: 'phone',
+    }
+  );
+  return Phone;
+};
