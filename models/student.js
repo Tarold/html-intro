37d7a84c456164752d93d589ff664ea8ae28b835
tarold <ltdecorer@gmail.com>
2023-04-09 16:03:18 +0300

    Add group model, students, seeders

--- models/student.js
+++ models/student.js
@@ -9,7 +9,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      // define association here
+      Student.belongsTo(models.Group, {
+        foreignKey: 'groupId',
+      });
     }
   }
   /* Обмеження
