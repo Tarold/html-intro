d9c7204aa18a786e086d8856c07bf95f3294c658
tarold <ltdecorer@gmail.com>
2023-04-09 19:50:14 +0300

    Add tasks endpoints

--- models/task.js
+++ models/task.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Task extends Model {
     static associate (models) {
@@ -8,6 +9,7 @@ module.exports = (sequelize, DataTypes) => {
       });
     }
   }
+
   Task.init(
     {
       body: {
