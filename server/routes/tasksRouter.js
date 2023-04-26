7f0c8291186fa40769c7f157e9e29b59dbf9be5b
tarold <ltdecorer@gmail.com>
2023-04-26 10:51:08 +0300

    Add tasks endpoints

--- server/routes/tasksRouter.js
+++ server/routes/tasksRouter.js
@@ -3,6 +3,18 @@ const { tasksController } = require('../controllers');
 
 const tasksRouter = Router();
 
-tasksRouter.get('/', tasksController.getTasks);
+tasksRouter
+  .route('/')
+  .get(tasksController.getTasks)
+  .post(tasksController.createTask);
+
+//TODO модернізувати основний сайт
+//або завдання окремою сторінкою, або як вспливаючий елмент
+tasksRouter
+  .route('/:taskId')
+  .get(tasksController.getTaskById)
+  .patch(tasksController.updateTaskById)
+  .put(tasksController.updateOrCreateTaskById, tasksController.createTask)
+  .delete(tasksController.deleteTaskById);
 
 module.exports = tasksRouter;
