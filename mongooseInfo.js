6a3a42223f9e67a2e7b0e183072ae869cceedcd7
tarold <ltdecorer@gmail.com>
2023-04-29 19:50:49 +0300

    Add mongoose CRUD ex

--- mongooseInfo.js
+++ mongooseInfo.js
@@ -11,6 +11,27 @@ const mongoose = require('mongoose');
     const testTask = { value: 'ToDo HW' };
 
     // const createdTask = await Task.create(testTask);
+    // Task.find
+    const foundTasks = await Task.find();
+    console.log('foundTasks :>> ', foundTasks);
+
+    // Task.findById
+    const foundTask = await Task.findById('644d468db277a42265e9d3ce');
+    console.log('foundTask :>> ', foundTask);
+
+    // Task.findByIdAndUpdate
+    const updatedTask = await Task.findByIdAndUpdate(
+      '644d468db277a42265e9d3ce',
+      { value: 'Rest' },
+      { new: true }
+    );
+    console.log('updatedTask :>> ', updatedTask);
+
+    // Task.findByIdAndDelete
+    const deletedTask = await Task.findByIdAndDelete(
+      '644d468db277a42265e9d3ce'
+    );
+    console.log('deletedTask :>> ', deletedTask);
   } catch (err) {
     console.log('err :>> ', err);
   }
