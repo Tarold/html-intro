48c83bcd8ac5beb957d77167b32124b31aa02905
tarold <ltdecorer@gmail.com>
2023-05-02 12:21:49 +0300

    Add phone model

---
+++ models/phone.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const { PHONE_VALIDATION_SCHEMA } = require('./../utils/validationSchemas');
+
+const phoneSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 2,
+    maxLength: 64,
+    validate: {
+      validator: v => PHONE_VALIDATION_SCHEMA.name.isValidSync(v),
+    },
+  },
+  brand: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 2,
+    maxLength: 64,
+    validate: {
+      validator: v => PHONE_VALIDATION_SCHEMA.brand.isValidSync(v),
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  user: {
+    type: mongoose.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Phone = mongoose.model('Phone', phoneSchema);
+
+module.exports = Phone;
