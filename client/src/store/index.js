764b5ff89d04867a885df1773fdef1560ed56370
tarold <ltdecorer@gmail.com>
2023-05-07 15:13:43 +0300

    Initial commit

--- client/src/store/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './rootReducer';
-import { bringStoreToSocket } from '../api/index';
-
-const store = configureStore({ reducer: rootReducer });
-
-bringStoreToSocket(store);
-
-export default store;
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const messageRouter = require('./messageRouter');
-
-const router = Router();
-
-router.use('/messages', messageRouter);
-
-module.exports = router;
