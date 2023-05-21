00a9b21bda14dd7bb51be2644b4b78b1ead11ee6
tarold <ltdecorer@gmail.com>
2023-05-21 18:14:02 +0300

    Add chai test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert')
 const { sum } = require('./../utils/math')
+const { expect } = require('chai')
 
 describe('Testing math.sum', function () {
   it('hould return 4 when 2:number + 2:number', function () {
@@ -7,5 +8,10 @@ describe('Testing math.sum', function () {
   })
   it('should return 4 when "2": string + "2": string', function () {
     assert.equal(sum('2', '2'), 4)
+  })
+  it('should return Infinity when Number.MAX_VALUE + Number.MAX_VALUE', function () {
+    const result = sum(Number.MAX_VALUE, Number.MAX_VALUE)
+    const expected = Infinity
+    expect(result).to.equal(expected)
   })
 })
